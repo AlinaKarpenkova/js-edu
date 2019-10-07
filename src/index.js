84307ba11905126afe9ba4a67534edce555223fa
unknown <karpenkova.alina12@gmail.com>
2019-10-07 22:08:39 +0300

    feat: complete js-edu task

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
 /**
- * @param preferences - target student focus
+ * @param focus - target student focus
  * @param knowsProgramming - if student can do programming and know basics
  * @param config - private student ability to perform for different focus modes
  * @returns number of weeks needed for finish education
  */
 module.exports = function getTimeForEducation(
-    focus = 'family', 
-    knowsProgramming = true,
-    config = {family: 4}
-    ) {
-      return 0;
-  };
-  
+  focus,
+  knowsProgramming,
+  config
+) {
+  return Math.ceil((knowsProgramming) ? 800 / config[focus] : 1300 / config[focus]);
+};
